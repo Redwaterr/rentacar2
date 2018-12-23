4cc95cfbf5ac3ea3405c8abd6c1405c08c8e32ed
Ugur <ugur@localhost.localdomain>
2018-12-23 15:15:45 +0300

    fuck

---
+++ server.js
@@ -0,0 +1,18 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/frontend2'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/frontend2/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080,() => {
+    console.log("Server is running on port 8000");
+});
